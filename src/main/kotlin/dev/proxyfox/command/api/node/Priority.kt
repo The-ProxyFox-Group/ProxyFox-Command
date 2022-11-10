e14b8ee4360e51105771557adef99437808b591a
Octal <59418903+oliver-makes-code@users.noreply.github.com>
2022-11-09 19:00:49 -0600

    a

--- src/main/kotlin/dev/proxyfox/command/api/node/Priority.kt
+++ src/main/kotlin/dev/proxyfox/command/api/node/Priority.kt
@@ -5,14 +5,17 @@ public enum class Priority(public val value: Int) {
      * Static string, never changes
      * */
     STATIC(3),
+
     /**
      * Limited charset
      * */
     SEMI_STATIC(2),
+
     /**
      * Non-limited charset
      * */
     VARIABLE(1),
+
     /**
      * Takes up rest of command
      * */
